0d01df657919494fef7442d81d745ae88c5641a8
yaroslavlevshunov <yaroslav.levshunov2@gmail.com>
2023-07-08 21:00:12 +0200

    mutex for tryToUpdateCurrencies

--- app/src/main/java/com/joy/yariklab/core/domain/interactor/CurrencyInteractorImpl.kt
+++ app/src/main/java/com/joy/yariklab/core/domain/interactor/CurrencyInteractorImpl.kt
@@ -5,6 +5,8 @@ import com.joy.yariklab.core.data.model.Currency
 import com.joy.yariklab.core.data.model.CurrencyDetails
 import com.joy.yariklab.core.domain.repository.CurrencyRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 private const val MAX_TIME_DIFF_FOR_DATA_UPDATE = 1000 * 60 * 60 * 12
 
@@ -12,18 +14,22 @@ class CurrencyInteractorImpl(
     private val currencyRepository: CurrencyRepository,
 ): CurrencyInteractor {
 
+    private val mutex = Mutex()
+
     override suspend fun subscribeOnCurrencies(): Flow<List<Currency>> {
         return currencyRepository.subscribeOnCurrencies()
     }
 
     override suspend fun tryToUpdateCurrencies() {
-        val isNeedToUpdateCurrencyCache = isNeedToUpdateCurrencyCache()
-        val params = when {
-            isNeedToUpdateCurrencyCache -> CacheUpdateDataParams.Update(System.currentTimeMillis())
-            else -> CacheUpdateDataParams.Leave
-        }
+        mutex.withLock {
+            val isNeedToUpdateCurrencyCache = isNeedToUpdateCurrencyCache()
+            val params = when {
+                isNeedToUpdateCurrencyCache -> CacheUpdateDataParams.Update(System.currentTimeMillis())
+                else -> CacheUpdateDataParams.Leave
+            }
 
-        currencyRepository.tryToUpdateCurrencies(params)
+            currencyRepository.tryToUpdateCurrencies(params)
+        }
     }
 
     override suspend fun getCurrencyByCode(code: String): CurrencyDetails {
