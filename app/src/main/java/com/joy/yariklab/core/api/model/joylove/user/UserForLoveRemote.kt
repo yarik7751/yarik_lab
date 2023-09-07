dedc02c0446507281f0022f9a22a3313c07a3aff
yaroslavlevshunov <yaroslav.levshunov2@gmail.com>
2023-09-07 12:37:04 +0300

    getusersforlove request

---
+++ app/src/main/java/com/joy/yariklab/core/api/model/joylove/user/UserForLoveRemote.kt
@@ -0,0 +1,28 @@
+package com.joy.yariklab.core.api.model.joylove.user
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserForLoveRemote(
+    @SerialName("age")
+    val age: Int,
+    @SerialName("contacts")
+    val contacts: String,
+    @SerialName("email")
+    val email: String,
+    @SerialName("id")
+    val id: Int,
+    @SerialName("logoPath")
+    val logoPath: String,
+    @SerialName("mobilePhone")
+    val mobilePhone: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("rating")
+    val rating: Double,
+    @SerialName("sex")
+    val sex: Int,
+    @SerialName("videoPath")
+    val videoPath: String,
+)
--- app/src/main/java/com/joy/yariklab/core/api/service/JoyLoveRemoteService.kt
+++ app/src/main/java/com/joy/yariklab/core/api/service/JoyLoveRemoteService.kt
@@ -4,13 +4,16 @@ import com.joy.yariklab.core.api.model.joylove.LoginParamsRemote
 import com.joy.yariklab.core.api.model.joylove.RegistrationParamsRemote
 import com.joy.yariklab.core.api.model.joylove.UploadedFileRemote
 import com.joy.yariklab.core.api.model.joylove.UserTokensRemote
+import com.joy.yariklab.core.api.model.joylove.user.UserForLoveRemote
 import okhttp3.MultipartBody
 import retrofit2.Retrofit
 import retrofit2.create
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Query
 
 interface JoyLoveRemoteService {
 
@@ -35,4 +38,12 @@ interface JoyLoveRemoteService {
     suspend fun uploadVideo(
         @Part image: MultipartBody.Part
     ): UploadedFileRemote
+
+    @GET("getusersforlove")
+    suspend fun getUsersForLove(
+        @Query("distance") distance: Int,
+        @Query("sex") sex: Int,
+        @Query("agemin") ageMin: Int,
+        @Query("agemax") ageMax: Int,
+    ): List<UserForLoveRemote>
 }
